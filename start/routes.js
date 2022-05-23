191f9cfab397730d0b5fed90aa9cbee3d3c61113
OCG071 <omarcg071@gmail.com>
2022-05-23 15:06:59 -0500

    Se sube y actualiza el proyecto

---
+++ start/routes.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.group(() =>{
+  Route.post('usuarios/registro','UserController.store');
+  Route.post('usuarios/login','UserController.login');
+  Route.get('proyectos','ProyectoController.index').middleware('auth');
+  Route.post('proyectos','ProyectoController.create').middleware('auth');
+  Route.delete('proyectos/:id','ProyectoController.destroy').middleware('auth');
+  Route.patch('proyectos/:id','ProyectoController.update').middleware('auth');
+}).prefix('api/v1/');
+
+Route.group(() =>{
+  Route.post('usuarios/registro','UserController.store');
+}).prefix('api/v2/');
